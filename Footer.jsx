de44618046726c08d2bb6e725d60b8149a9da8e5
saipavan47 <43907719+saipavan47@users.noreply.github.com>
2021-06-07 11:34:47 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+
+
+  return (
+  
+  <footer>
+  <p>Copyright @ 2021</p> 
+    </footer>
+  );
+}
+export default Footer;
+
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note(){
+  return (
+  <div className="note">
+    <h1>Javascript and React.js</h1>
+  <p>This was an amazing bootcamp taken by Shaurya sinha
+    We covered everything from scratch including javascript 
+    and React Js. I absolutely loved the sessions. The teaching was Awesome.
+  </p>
+  </div>
+  );
+}
+export default Note;
+
